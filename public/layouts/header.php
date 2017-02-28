5d1b5321aa0885b1b95aa23813e76894c4dd3dd3
raisulru <raisulru10@gmail.com>
2017-02-28 18:20:09 +0600

    This is a php photogallary with mailing and comment

---
+++ public/layouts/header.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>Photo Gallery</title>
+    
+    <link href="stylesheets/bootstrap.min.css" media="all" rel="stylesheet" type="text/css" />
+<link href="stylesheets/main.css" media="all" rel="stylesheet" type="text/css" />
+  </head>
+  <body class="container">
+    <div id="header">
+      <h1>Photo Gallery</h1>
+    </div>
+    <div id="main">
